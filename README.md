5ed22a7ceb0f44bb9d448541ba600c0061afa8af
Neo-Hao <qianghaoqiang@gmail.com>
2023-12-24 10:38:56 -0800

    add Readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# About this project
+
+This project is aboud building a classic to-do list application inspired by https://todomvc.com/. 
+
+<img src="./design/demo-1.png" width="500" />
+
+# Backend
+
+The backend of this project is in the `backend` folder. It is implemented with Node.js and Express.js. The database is MongoDB.
+
+To run the backend, you need to navigate to the `backend` folder first, and follow the following steps:
+
+1. install all the dependencies by running `npm install`
+2. configure your MongoDB database, and copy/paste the connection string into the `.env` file. In other words, you need to replace the `ATLAS_URI` value with your own connection string. When doing so, please make sure that you have replaced the `<username>` and `<password>` part with your own.
+3. start the backend by runnning `npm start`.
+
+# Frontend
+
+The frontend of this project is in the `frontend` folder. Before you run the frontend, please make sure that you have started the backend first. The backend should run without issues on port 8000.
+
+To run the frontend, you can start the project with `npm start`.
+
+To test the frontend, you can run `npm test`.
+
--- frontend/cypress/e2e/basic-testing/todo.cy.js
+++ frontend/cypress/e2e/basic-testing/todo.cy.js
@@ -16,18 +16,6 @@ describe('example to-do app', () => {
     cy.visit('http://127.0.0.1:3000/');
   });
 
-  it('displays two todo items by default', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `contains` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('.todos li').should('have.length', 2);
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    cy.get('.todo-list li label').first().contains('See the doctor');
-    cy.get('.todo-list li label').last().contains('Walk the dog');
-  });
-
   it('can add and delete todo items', () => {
     // We'll store our item text in a variable so we can reuse it
     const newItem = 'Feed the dog';
@@ -36,14 +24,14 @@ describe('example to-do app', () => {
     cy.get('input[placeholder="What needs to be done?"]').type(`${newItem}`).type('{enter}');
 
     // verify that the newItem was added to the list
-    cy.get('.todo-list li label').should('have.length', 3).last().contains(newItem);
+    cy.get('.todo-list li label').should('have.length', 1).last().contains(newItem);
 
     // delete newItem from list
     cy.get('.todo-list li button').last().click();
 
     // verify that the newItem was deleted from the list
-    cy.get('.todos li').should('have.length', 2);
-    cy.get('.todo-list li label').first().contains('See the doctor');
-    cy.get('.todo-list li label').last().contains('Walk the dog');
+    cy.get('.todos li').should('have.length', 0);
+    // cy.get('.todo-list li label').first().contains('See the doctor');
+    // cy.get('.todo-list li label').last().contains('Walk the dog');
   });
 });
